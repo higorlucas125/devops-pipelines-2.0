a92eb1a4a700fc4317f7e4fafcfb7bcbee480b24
HigorLucas_etica <higor_etica@algartelecom.com.br>
2025-10-04 15:35:34 -0300

    fix: force pipe

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ console.log('This is a sample JavaScript file. To CI/D, please run the tests.');
 console.log('This is a sample JavaScript file. To CI/D, please run the tests.');
 console.log('This is a sample JavaScript file. To CI/D, please run the tests.');
 console.log('This is a sample JavaScript file. To CI/D, please run the tests.');
+console.log('This is a sample JavaScript file. To CI/D, please run the tests.');
 

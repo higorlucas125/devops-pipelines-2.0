4fcbc634d6acfc7c3916181e01bb2d1aaf085d14
HigorLucas_etica <higor_etica@algartelecom.com.br>
2025-10-05 22:29:37 -0300

    fix: test

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
 console.log('Hello, World!');
 console.log('This is a sample JavaScript file. To CI/D, please run the tests.');
-console.log('This is a sample JavaScript file. To CI/D, please run the tests.');
-console.log('This is a sample JavaScript file. To CI/D, please run the tests.');
-console.log('This is a sample JavaScript file. To CI/D, please run the tests.');
-console.log('This is a sample JavaScript file. To CI/D, please run the tests.');
-console.log('This is a sample JavaScript file. To CI/D, please run the tests.');
-console.log('This is a sample JavaScript file. To CI/D, please run the tests.');
